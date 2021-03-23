354c3a0b914de2d1c173d35a59e6377be09eeb70
atharvawafgaonkar <79642051+atharvawafgaonkar@users.noreply.github.com>
2021-03-23 19:26:51 +0530

    Update project.js

--- project.js
+++ project.js
@@ -19,7 +19,7 @@ var brickPadding = 10;
 var brickOffsetTop = 30;
 var brickOffsetLeft = 30;
 var score = 0;
-var lives = 4000000;
+var lives = 4;
 
 var bricks = [];
 for(var c=0; c<brickColumnCount; c++) {
